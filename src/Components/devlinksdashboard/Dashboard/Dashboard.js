14c0f7afc24ba9dbb2ed3a56830fd6fa70f153d5
Priyal lohar <priyallohar27@gmail.com>
2023-09-20 16:16:54 +0530

    working of profile view

--- src/Components/devlinksdashboard/Dashboard/Dashboard.js
+++ src/Components/devlinksdashboard/Dashboard/Dashboard.js
@@ -2,12 +2,14 @@ import React from 'react'
 import DashboardHeader from '../DashboardHeader/DashboardHeader'
 import Links from '../Links/Links'
 import"./Dashboard.scss"
+import Preview from "../Preview/Preview"
 
 const Dashboard = () => {
   return (
     <div className='dashboard-container'>
         <DashboardHeader></DashboardHeader>
-        <Links></Links>
+        {/* <Links></Links> */}
+        <Preview></Preview>
     </div>
   )
 }
