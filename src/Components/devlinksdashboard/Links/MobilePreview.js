1947963b46838d236d85b9c300bcc6a370da1f3f
Priyal lohar <priyallohar27@gmail.com>
2023-09-25 17:18:42 +0530

    added phone mocke and profile detail container and style it

--- src/Components/devlinksdashboard/Links/MobilePreview.js
+++ src/Components/devlinksdashboard/Links/MobilePreview.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./MobilePreview.scss"
 const MobilePreview = () => {
   return (
-    <div>
+    <div className='outer-phone-mockup-container'>
         <div className="Phone-mockup">
         <svg
           xmlns="http://www.w3.org/2000/svg"
