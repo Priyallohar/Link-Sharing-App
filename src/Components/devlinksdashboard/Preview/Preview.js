31ccdcf1a5901d9889014c5bbeb89cbf5cb60b63
Priyal lohar <priyallohar27@gmail.com>
2023-09-27 16:46:32 +0530

    added imahe add container

--- src/Components/devlinksdashboard/Preview/Preview.js
+++ src/Components/devlinksdashboard/Preview/Preview.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Preview.scss"
 import MobilePreview from '../Links/MobilePreview'
 
 const Preview = () => {
+    const [selectedPhotos, setSelectedPhotos] = useState([]);
+
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setSelectedPhotos([...selectedPhotos, ...files]);
+  };
+
   return (
     <div className='preview-outer-container'>
          <MobilePreview></MobilePreview>
-         {/* <div className='outer-personal-details'> */}
          <div className='personal-details'>
          <div className="header-part">
             <h2>Profile Details</h2>
@@ -14,7 +20,14 @@ const Preview = () => {
             Add your details to create a personal touch to your profile.
             </p>
           </div>
-          {/* </div> */}
+
+          <div className='add-profile-pic-container'>
+            <text>Profile Picture</text>
+            <input type='file' accept='image/*' onChange={handleFileChange} />
+            <p>Image must be below 1024x1024px. Use PNG or JPG format.</p>
+          </div>
+
+          
          </div>
     </div>
   )
