9261c886c9203373b046e8d7cfe98abaaeb390e6
Priyal lohar <priyallohar27@gmail.com>
2023-09-29 19:10:32 +0530

    changes in profileDetails

--- src/Components/devlinksdashboard/Dashboard/Dashboard.js
+++ src/Components/devlinksdashboard/Dashboard/Dashboard.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import DashboardHeader from '../DashboardHeader/DashboardHeader'
-import Links from '../Links/Links'
-import"./Dashboard.scss"
-import Preview from "../Preview/Preview"
+import React from "react";
+import DashboardHeader from "../DashboardHeader/DashboardHeader";
+import Links from "../Links/Links";
+import "./Dashboard.scss";
+import Preview from "../Preview/Preview";
+import ProfileDetails from "../ProfileDetails/ProfileDetails"
 
 const Dashboard = () => {
   return (
-    <div className='dashboard-container'>
-        <DashboardHeader></DashboardHeader>
-        {/* <Links></Links> */}
-        <Preview></Preview>
+    <div className="dashboard-container">
+      <DashboardHeader></DashboardHeader>
+      {/* <Links></Links> */}
+      <ProfileDetails></ProfileDetails>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
---
+++ src/Components/devlinksdashboard/ProfileDetails/ProfileDetails.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+import "./ProfileDetails.scss"
+import MobilePreview from '../Links/MobilePreview'
+
+const Preview = () => {
+    const [selectedPhotos, setSelectedPhotos] = useState([]);
+
+  const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setSelectedPhotos([...selectedPhotos, ...files]);
+  };
+
+  return (
+    <div className='preview-outer-container'>
+         <MobilePreview></MobilePreview>
+         <div className='personal-details'>
+         <div className="header-part">
+            <h2>Profile Details</h2>
+            <p>
+            Add your details to create a personal touch to your profile.
+            </p>
+          </div>
+
+          <div className='add-profile-pic-container'>
+            <text>Profile Picture</text>
+            <div className='upload-continer'>
+            <button><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#633CFF" d="M33.75 6.25H6.25a2.5 2.5 0 0 0-2.5 2.5v22.5a2.5 2.5 0 0 0 2.5 2.5h27.5a2.5 2.5 0 0 0 2.5-2.5V8.75a2.5 2.5 0 0 0-2.5-2.5Zm0 2.5v16.055l-4.073-4.072a2.5 2.5 0 0 0-3.536 0l-3.125 3.125-6.875-6.875a2.5 2.5 0 0 0-3.535 0L6.25 23.339V8.75h27.5ZM6.25 26.875l8.125-8.125 12.5 12.5H6.25v-4.375Zm27.5 4.375h-3.34l-5.624-5.625L27.91 22.5l5.839 5.84v2.91ZM22.5 15.625a1.875 1.875 0 1 1 3.75 0 1.875 1.875 0 0 1-3.75 0Z"/></svg> <div>+ Upload Image</div></button>
+            {/* <input type='file' accept='image/*' onChange={handleFileChange} /> */}
+            <p>Image must be below 1024x1024px. Use PNG or JPG format.</p>
+            </div>
+          </div>
+          <div className='info-container'>
+              <div className='input-div'><label>First Name </label>  <input type="text" placeholder='e.g. John'/></div> 
+              <div className='input-div'> <label>Last Name  </label> <input type="text" placeholder='e.g. Appleseed'/></div> 
+              <div className='input-div'> <label> Email </label><input type="email" placeholder='e.g. email@example.com'/></div> 
+        </div>
+        <div className="saveBtn-container">
+          <button>Save</button>
+        </div>
+         </div>
+         
+    </div>
+  )
+}
+
+export default Preview
